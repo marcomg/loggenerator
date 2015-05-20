ad2a43206cef3c0f80095494a137c09ea4985295
s3v <s3v@s3v3land.lan>
2015-05-20 18:34:06 +0200

    add hide() function

--- libs/fileHandler.py
+++ libs/fileHandler.py
@@ -2,6 +2,8 @@
 
 import os
 import loggenerator
+import constants
+import re
 
 class FileOps():
     
@@ -13,7 +15,7 @@ class FileOps():
         self.f = open(self.filename, 'w')
         
     def write(self):
-        self.f.write(loggenerator.getLogFile())
+        self.f.write(self.hide())
         
     def close(self):
         self.f.close
@@ -22,8 +24,17 @@ class FileOps():
     def chmod(self):
         os.chmod(self.filename, 666)
         
+    # hide away the username and hostname from log file
+    def hide(self):
+        myloggenerator = loggenerator.functions()
+        myvar = myloggenerator.getLogFile()
+        myvar = re.sub(r'\b' + constants.utente + r'\b', 'nomeutente', myvar)
+        myvar = re.sub(r'\b' + constants.nomehost + r'\b', 'nomehost', myvar)
+        return myvar
+        
     def go(self):
         self.open()
+        self.hide()
         self.write()
         self.close()
         self.chmod()
